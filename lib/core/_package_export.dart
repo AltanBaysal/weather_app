c61f7d91ae572da8a7c31e9ae302ed29b786a4ae
AltanBaysal <96086932+AltanBaysal@users.noreply.github.com>
2023-04-19 19:36:48 +0300

    packages added

---
+++ lib/core/_package_export.dart
@@ -0,0 +1,7 @@
+export 'package:equatable/equatable.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:get_it/get_it.dart';
+export 'package:internet_connection_checker/internet_connection_checker.dart';
+export 'package:dio/dio.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:riverpod/riverpod.dart';
